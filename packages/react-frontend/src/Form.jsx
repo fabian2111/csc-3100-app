ae5d4c2c27a9613e896da0144210fbd5e6154ce9
fabian2111 <limonespe5@gmail.com>
2026-08-30 17:04:38 -0700

    IE1 Final commit - working forms

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
+   const [person, setPerson] = useState({
+    name: "",
+    job: "",
+  });
+
     function handleChange(event) {
     const { name, value } = event.target;
     if (name === "job") setPerson({ name: person["name"], job: value });
     else setPerson({ name: value, job: person["job"] });
     }
 
-  const [person, setPerson] = useState({
-    name: "",
-    job: "",
-  });
+  function submitForm(){
+    props.handleSubmit(person);
+    setPerson({ name: "", job: "" })
+  }
 
   return (
   <form>
@@ -30,6 +35,7 @@ function Form() {
       value={person.job}
       onChange={handleChange}
     />
+    <input type="button" value="Submit" onClick={submitForm} />
   </form>
 );
 
