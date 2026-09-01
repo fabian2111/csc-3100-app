481bd4d12e987de2c82267436c82b5d92d98e5e3
fabian2111 <limonespe5@gmail.com>
2026-08-31 22:02:56 -0700

    added a post method to add users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,6 +42,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -68,6 +73,12 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-})
+});
