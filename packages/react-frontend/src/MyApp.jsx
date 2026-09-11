7ba5abb9a9a18fa744190735ce7ae8c96b6cd189
fabian2111 <limonespe5@gmail.com>
2026-09-10 18:05:55 -0700

    updated updateList function to add users to backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -10,6 +10,17 @@ function MyApp() {
       return promise;
     }
 
+    function postUser(person) {
+      const promise = fetch("http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+      });
+      return promise;
+    }
+
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
             return i !== index;
@@ -18,7 +29,12 @@ function MyApp() {
     }
 
     function updateList(person){
-        setCharacters([...characters, person])
+        postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        });
+
     }
 
     useEffect(() => {
